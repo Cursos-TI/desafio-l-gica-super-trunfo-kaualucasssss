4d5111745785fe022cc9401995abb129e190b54e
kaualucasssss <kauajg667@gmail.com>
2025-02-26 18:00:23 +0000

    a

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,43 +1,113 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
+int main(){
 
-int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
-
+        char estado1, estado2;
+        char codigo1[4], codigo2[4];
+        char nome_cidade1[45], nome_cidade2[45];
+       unsigned long int populacao1, populacao2;
+        float  area1, area2;
+        float pib, pib2;
+        int num_ponto_turistico1, num_ponto_turistico2,poder,poder4,poder2,poder3;
+        float divisao1, divisao2, divi3, divi4;
+        float total1, totaL2;
+        float superpoder1, superpoder2;
+       
+         printf("---- CADASTRO CARTA 1 ----\n\n");
+                
+            printf("--Digite o estado (Uma letra de 'A' a 'b'): \n");
+            scanf(" %c", &estado1);
+                
+            printf("--Digite o código da cidade (A letra do estado e um número de 01 a 04): \n");
+            scanf("%s", codigo1);
+            printf("--Digite o nome da cidade: \n");
+            scanf(" %s[^\n]", nome_cidade1);
     
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
+            printf("--Digite a população da cidade: \n");
+            scanf("%lu", &populacao1);
+    
+            printf("--Digite a area da cidade em KM2: \n");
+            scanf("%f", &area1);
+    
+            printf("--Digite o PIB da cidade em bilhões: \n");
+            scanf("%f", &pib);
+    
+            printf("--Digite o número de pontos turísticos: \n");
+            scanf("%d", &num_ponto_turistico1);
 
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
+           divisao1  = (populacao1 + area1) / 2;
 
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
+           divi3 = (pib+populacao1) / 2;
 
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
+            printf("\n--Carta 1--\n\n");
+            printf("-Estado: %c\n", estado1);
+            printf("-Código da cidade: %s\n", codigo1);
+            printf("-Nome da Cidade: %s\n", nome_cidade1);
+            printf("-População da cidade: %lu\n", populacao1);
+            printf("-Área da cidade em km²: %.2f KM²\n", area1);
+            printf("-PIB da cidade em bilhões de reais: %.2f bilhões de reais\n", pib);
+            printf("-Número de pontos turísticos: %d\n", num_ponto_turistico1);
+            printf("-Densidade populacional: %.2f hab/km²\n", divisao1);
+            printf("-PIB per capita: %.2f reais\n", divi3);
+            superpoder1= area1+ populacao1 + pib + num_ponto_turistico1 + (populacao1/area1) + (populacao1/pib);
+           printf("super força: %.2f\n\n", superpoder1);
 
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
+           printf("---- CADASTRO CARTA 2 ----\n\n");
+                
+            printf("--Digite o estado (Uma letra de 'A' a 'b'): \n");
+            scanf(" %c", &estado2);
+                
+            printf("--Digite o código da cidade (A letra do estado e um número de 01 a 04): \n");
+            scanf("%s", codigo2);
+          
+            printf("--Digite o nome da cidade: \n");
+            scanf(" %s[^\n]", nome_cidade2);
+    
+            printf("--Digite a população da cidade: \n");
+            scanf("%lu", &populacao2);
+    
+            printf("--Digite a area da cidade em KM2: \n");
+            scanf("%f", &area2);
+    
+            printf("--Digite o PIB da cidade em bilhões: \n");
+            scanf("%f", &pib2);
+    
+            printf("--Digite o número de pontos turísticos: \n");
+            scanf("%d", &num_ponto_turistico2);
 
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
+            divisao2  = (populacao2 + area2) / 2;
 
-    return 0;
-}
+            divi4 = (pib2+populacao2) / 2;
+           
+           
+        printf("\n--Carta 2--\n\n");
+            printf("-Estado: %c\n", estado2);
+            printf("-Código da cidade: %s\n", codigo2);
+            printf("-Nome da Cidade: %s\n", nome_cidade2);
+            printf("-População da cidade: %lu\n", populacao2);
+            printf("-Área da cidade em km²: %.2f KM²\n", area2);
+            printf("-PIB da cidade em bilhões de reais: %.2f bilhões de reais\n", pib2);
+            printf("-Número de pontos turísticos: %d\n", num_ponto_turistico2);
+            printf("-Densidade populacional: %.2f hab/km²\n", divisao2);
+            printf("-PIB per capita: %.2f reais\n", divi4);
+            superpoder2= area2+ populacao2 + pib2 + num_ponto_turistico2 + (populacao2/area2) + (populacao2/pib2);
+            printf("força total: %.2f\n\n\n", superpoder2);
+
+            printf("---- resultado ----\n\n");
+           
+            printf(" força total da carta1: %.2f\n", superpoder1);
+            printf(" força total da carta2: %.2f\n\n", superpoder2);
+            if (superpoder1>superpoder2)        
+           {printf("  - carta 1 venceu - \n");
+            }
+            else("  - carta 2 venceu - \n");
+            
+            if (superpoder1 == superpoder2)
+            {printf(" - empate - ");
+              /* code */
+            }
+            
+          
+            }
+            
+
